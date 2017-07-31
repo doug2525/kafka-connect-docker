7a77f077a3bb2f7ea8d4cdcc47806b4dc3bc654b
sbkoth <sbkoth@users.noreply.github.com>
2017-07-31 13:56:37 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,2 +1,3 @@
 envsubst < /etc/kafka/connect-standalone.properties.template > /etc/kafka/connect-standalone.properties
 envsubst < /etc/kafka-connect-jdbc/jdbc-source.properties.template > /etc/kafka-connect-jdbc/jdbc-source.properties
+envsubst < /etc/kafka-connect-jdbc/jdbc-sink.properties.template > /etc/kafka-connect-jdbc/jdbc-sink.properties
