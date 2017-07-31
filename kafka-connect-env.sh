290f80f508045cc39cca426b7da148cea18bf540
sbkoth <sbkoth@users.noreply.github.com>
2017-07-31 13:53:25 -0400

    Update kafka-connect-env.sh

--- kafka-connect-env.sh
+++ kafka-connect-env.sh
@@ -1,5 +1,11 @@
+#### Command to execute
+
+KC_CMD=
+## Example: /usr/bin/connect-standalone /etc/kafka/connect-standalone.properties /etc/kafka-connect-jdbc/jdbc-sink.properties
+
+
 #### Heap setting for Connector
-KAFKA_HEAP_OPTS="-Xmx 1536m -Xms 1024m"
+#KAFKA_HEAP_OPTS="-Xmx1536m -Xms1024m"
 
 #### Kafka Connect Kafka Related Properties
 
@@ -14,7 +20,9 @@ VALUE_SR_URL=
 
 # Offsets file
 
-KC_OFFSETS_FILE=connect-offsets
+KC_OFFSETS_FILE=
+
+## Absolute path within the Docker container. use host networking when possible, otherwise may not be reachable.
 
 # SSL Properties
 
@@ -24,13 +32,21 @@ SSL_TRUSTSTORE_PW=
 FULL_PATH_TO_TS=
 FULL_PATH_TO_KS=
 
-
 #### JDBC source related properties
 
-CONNECTOR_NAME=
-JDBC_URL=
-MODE=
-QUERY=
-TOPIC=test
-POLL_INTERVAL_MS=
-TIMESTAMP_COLUMN_NAME=
+SOURCE_CONNECTOR_NAME=
+SOURCE_JDBC_URL=
+SOURCE_MODE=
+SOURCE_QUERY=
+SOURCE_TOPIC=
+SOURCE_POLL_INTERVAL_MS=
+SOURCE_TIMESTAMP_COLUMN_NAME=
+
+
+#### JDBC Sink related properties
+
+SINK_CONNECTOR_NAME=
+SINK_TOPICS=
+SINK_AUTO_CREATE=
+SINK_AUTO_EVOLVE=
+SINK_TABLE_NAME_FORMAT=
